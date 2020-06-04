53c4cc9865ad9bf1fb917967d6c3060700dc5e2f
lupuilie <lupuiliealexandru@gmail.com>
2020-06-04 18:45:49 +0300

    Edit

--- snake/js/gameplay.js
+++ snake/js/gameplay.js
@@ -5,24 +5,26 @@ const SPEED = [
 	3,
 	3.5,
 	4,
-	4.5
+	4.5,
+	5
 ];
 const POINT = [
 	3,
 	4,
 	5,
 	10,
-	15
+	15,
+	20
 ];
 
 let currentScore = 0;
 export function updatePoints() {
 	currentScore = getSnakeLength() -1;
 	$('#score').text(currentScore);
-	
+
 	POINT.some(function(currentValue, index) {
 		if(currentValue == currentScore) {
 			setSnakeSpeed(SPEED[index]);
-		} 
+		}
 	});
 }
